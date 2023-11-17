ae5a81cf0667571422ca5f3e7ead27b0e8639132
Jumi <feliciastrandberg@gmail.com>
2023-11-17 11:45:56 +0100

    refined position and size of eye images on larger screen

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,49 @@
-// Get the eyeContainer from HTML
-
 // Show open eyes img on mouse hover
 function handleMouseEnter() {
-  const images = eyeContainer.querySelectorAll('img');
+  const images = eyeContainer.querySelectorAll("img");
   images[0].style.opacity = 0;
   images[1].style.opacity = 1;
 }
 
-// Hide open eyes img on mouse leave.
+// Hide open eyes img on mouse leave
 function handleMouseLeave() {
-  const images = eyeContainer.querySelectorAll('img');
+  const images = eyeContainer.querySelectorAll("img");
   images[0].style.opacity = 1;
   images[1].style.opacity = 0;
 }
 
 
+// Listen for correct mouse position and call functions 
+function activateHover() {
+  const eyeContainer = document.getElementById("eyeContainer");
 
-function onlyDesktop() {
-  const desktopSize = window.matchMedia('(min-width: 1024px)').matches; 
-  
-  if (desktopSize) {
-    const eyeContainer = document.getElementById('eyeContainer');
+  function handleMouseMove(event) {
+    const mouseY = event.clientY;
+    const mouseX = event.clientX;
 
-    eyeContainer.addEventListener('mousemove', function(event) {
-      const mouseY = event.clientY;
-      const mouseX = event.clientX
-    
-      if (mouseY <= 250 && mouseX <= 250) {
-        handleMouseEnter();
-      } else {
-        handleMouseLeave();
-      }
-    });
+    if (mouseY <= 250 && mouseX <= 250) {
+      handleMouseEnter();
+    } else {
+      handleMouseLeave();
+    }
   }
+
+  // Match the media query in the CSS
+  const desktopQuery = window.matchMedia("(min-width: 1024px)");
+
+  // only do if window size is right
+  function handleViewportChange() {
+    if (desktopQuery.matches) {
+      eyeContainer.addEventListener("mousemove", handleMouseMove);
+    } else {
+      eyeContainer.removeEventListener("mousemove", handleMouseMove);
+      handleMouseLeave(); 
+    }
+  }
+
+  handleViewportChange(); 
+
+  desktopQuery.addListener(activateHover); 
 }
 
-onlyDesktop();
+activateHover();
