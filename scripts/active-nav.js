10804db9bd78a90870066455e5d404644e5a78c4
Jumi <feliciastrandberg@gmail.com>
2023-11-23 22:14:15 +0100

    nav active class script works for scroll position now too. Might need to tweak threshold. Will come back when layout for whole page is complete

--- scripts/active-nav.js
+++ scripts/active-nav.js
@@ -19,6 +19,41 @@ navLinks.forEach(function(link) {
 })
 
 // For scroll:
+// Found info about intersection observer API from mdn web docs. Used the examples and templates for this part of the script.
+
 // Get all the sections 
-const navSections = document.querySelectorAll('main > div');
-console.log(navSections);
+const navSections = document.querySelectorAll('section');
+
+// Create an object with the options for the IntersectionObservers
+const options = {
+    // Default browser viewport
+    root: null,
+    // Don't need I think
+    rootMargin: '0px',
+    // How many percent the user can see when the observer's callback should be called
+    threshold: 0.6
+}; 
+
+// The callback function that is called when a section enters or leaves view
+function handleIntersect(entries) {
+
+    // Add when intersecting otherwise remove
+    entries.forEach(function(entry) {
+        // Got this to identify which link from chatGPT 
+        const navLink = document.querySelector(`.nav-li a[href="#${entry.target.id}"]`);
+
+        if(entry.isIntersecting) {
+            navLink.classList.add('active-nav');
+        } else {
+            navLink.classList.remove('active-nav');
+        }
+    });
+}
+
+// Create a new IntersectionObserver instance with 'new' keyword that takes the callback function and the options from the object
+const observer = new IntersectionObserver(handleIntersect, options);
+
+// Observe every section 
+navSections.forEach(function(section) {
+    observer.observe(section);
+});
