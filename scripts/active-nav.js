c9b26dcac91d0c6cd84e93f139738debb832959b
Jumi Strandberg <feliciastrandberg@gmail.com>
2023-11-28 14:12:00 +0100

    tried fixing the error in the console

--- scripts/active-nav.js
+++ scripts/active-nav.js
@@ -14,21 +14,22 @@ const options = {
     root: null,
     // Don't need I think
     rootMargin: '0px',
-    // How many percent the user can see when the observer's callback should be called
+    // How many percent thes user can see when the observer's callback should be called
     threshold: 0.2
-}; 
+};
 
 // The callback function that is called when a section enters or leaves view
 function handleIntersect(entries) {
 
     // Add when intersecting otherwise remove
-    entries.forEach(function(entry) {
+    entries.forEach(function (entry) {
         const sectionId = entry.target.getAttribute('id');
+        const link = document.querySelector('.nav-li a[href="#' + sectionId + '"]');
 
-        if(entry.isIntersecting) {
-            navLinks.forEach(function(links) {
+        if (entry.isIntersecting) {
+            navLinks.forEach(function (links) {
                 links.classList.remove('active-nav');
-                document.querySelector('.nav-li a[href="#' + sectionId + '"]').classList.add('active-nav');
+                link.classList.add('active-nav')
             })
         } else {
             // navLink.classList.remove('active-nav');
@@ -40,7 +41,7 @@ function handleIntersect(entries) {
 const observer = new IntersectionObserver(handleIntersect, options);
 
 // Observe every section 
-navSections.forEach(function(section) {
+navSections.forEach(function (section) {
     observer.observe(section);
 });
 
